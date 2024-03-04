a43931283945454842d46de296d0c2ef904a6328
Purwa <purwadarozatunakis@gmail.com>
2024-03-04 09:49:24 +0700

    add checking use ldap

--- apps/auth/AuthController.go
+++ apps/auth/AuthController.go
@@ -9,10 +9,12 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func Login(c *gin.Context) {
 
+	config, _ := deps.LoadConfig(".")
 	var loginRequest dto.LoginRequest
 	if err := c.ShouldBind(&loginRequest); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
@@ -21,28 +23,54 @@ func Login(c *gin.Context) {
 	// deps.DB.Create(&user_models.User{Name: "Javan", Email: "purwa232@javan.co.id"})
 	var person user_models.User
 
-	ok, user, err := deps.AuthenticateLdapUser(loginRequest)
-	if err != nil {
-		c.JSON(500, gin.H{"message": "ERROR LDAP", "error": err})
-		return
-	}
-	if !ok {
-		c.JSON(401, gin.H{"message": "User not found"})
-		return
+	if config.AUTH_USE_LDAP == "false" {
+
+		if err := databases.DB.Where(user_models.User{Email: loginRequest.Username}).First(&person).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"message": "User not found"})
+			return
+		}
+		errorCompare := bcrypt.CompareHashAndPassword([]byte(person.Password), []byte(loginRequest.Password))
+		if errorCompare != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"message": "Password not match"})
+			return
+		}
+
+		email := person.Email
+		name := person.Name
+		givenToken, err := deps.CreateToken(email, name)
+
+		if (err) == nil {
+			c.JSON(200, gin.H{"message": "SUCCESS", "token": givenToken})
+			return
+		}
+
+	} else {
+		ok, user, err := deps.AuthenticateLdapUser(loginRequest)
+		if err != nil {
+			c.JSON(500, gin.H{"message": "ERROR LDAP", "error": err})
+			return
+		}
+		if !ok {
+			c.JSON(401, gin.H{"message": "User not found"})
+			return
+		}
+
+		if err := databases.DB.Where(user_models.User{Email: user["mail"]}).First(&person).Error; err != nil {
+			fmt.Println("Record not found", "creating new user")
+			databases.DB.Create(&user_models.User{Name: user["givenName"], Email: user["mail"]})
+		}
+
+		email := user["mail"]
+		name := user["givenName"]
+		givenToken, err := deps.CreateToken(email, name)
+
+		if (err) == nil {
+			c.JSON(200, gin.H{"message": "SUCCESS", "token": givenToken})
+			return
+		}
+
 	}
 
-	if err := databases.DB.Where(user_models.User{Email: user["mail"]}).First(&person).Error; err != nil {
-		fmt.Println("Record not found", "creating new user")
-		databases.DB.Create(&user_models.User{Name: user["givenName"], Email: user["mail"]})
-	}
-	token, err := deps.CreateToken(user["mail"], user["givenName"])
-	if (err) == nil {
-		c.JSON(200, gin.H{"message": "SUCCESS", "token": token})
-		return
-	} else {
-		c.JSON(500, gin.H{"message": "TOKEN_GENERATE_ERROR ", "token": token})
-		return
-	}
 }
 
 func Profile(c *gin.Context) {
